203ada02a02dd6e4e1d2925ed1415aeec3c6bbd8
Muhammad Faizan <faizan.aqeel814@gmail.com>
2023-08-17 16:21:26 +0500

    SMIT

---
+++ script.js
@@ -0,0 +1,12 @@
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+
+prevButton.addEventListener('click', () => {
+    // Code to handle previous section
+    console.log('Previous section');
+});
+
+nextButton.addEventListener('click', () => {
+    // Code to handle next section
+    console.log('Next section');
+});
